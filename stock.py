cf590ea46f5cf34435cadfdf02aec43f443a8723
Deebling5 <Abhijay9@ymail.com>
2020-12-30 00:20:06 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -1,4 +1,6 @@
+import streamlit as st
 import pandas as pd
+import quandl as q
 import requests
 import plotly.graph_objects as go
 from bs4 import BeautifulSoup
@@ -7,17 +9,15 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import os
+from twilio.rest import Client
 from selenium.webdriver.common.keys import Keys 
+import pandas as pd
 
 ## ET Now Feed Scrapper
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
-# Now you can start using Selenium
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+driver = webdriver.Chrome(r"C:\Users\91801\Desktop\Python Learning\Stock Indigator -Abhijay\chromedriver.exe", options = options)    
 driver.get("https://economictimes.indiatimes.com/markets/stocks/recos")  
 driver.execute_script("window.scrollTo(0, 6080)") 
 time.sleep(3)
@@ -33,8 +33,9 @@ for x in range(45):
     
 Str = ' '.join([str(elem) for elem in L])
 
-#df = pd.DataFrame(L, columns=['Recos'])
+df = pd.DataFrame(L, columns=['Recos'])
 #print (df)
 
 for x in range(45):
     print(L[x])
+st.dataframe(df)
